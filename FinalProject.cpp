54be92745ccdc8f6010b209443785f14ad304615
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2022-12-22 12:24:19 +0700

    Struktur Program Databook Perpustakaan
    tanda // dibuat untuk melanjutkan program

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -1,13 +1,185 @@
-#include <iostream>
-#include <string.h>
 #include <algorithm>
+#include <iostream>
+#include <vector>
 #include <string>
+#define MAX 20
+
+bool isTrue = false;
+int flag = 0;
+
 using namespace std;
 
-int main() {
+struct perpustakaan{
+    string judul_buku;
+    string penerbit;
+    string penulis;
+    int tahun_terbit;
+    float rating;
+    string status;
+}; struct perpustakaan perpus[MAX]; 
 
-cout << "Final Procject ";
+struct Search_Engine{
+    string judul_buku;
+    string tags;
+    float rating;
+}; vector<Search_Engine> SEO;
 
+void data_buku(){
+   
+    // Buat data buku sampai 20 buku
+}
 
+void data_tags(){
+    // Judul buku, Tags (dipisahkan dengan koma), rating
 
+    // Buat data buku sampai 20 buku
+}
+
+void searching(){
+    // Membuat fitur pencarian
+}
+
+void searching_tags(){
+    // Membuat fitur pencarian berdasarkan tags
+}
+
+void sorting(){
+    // Membuat sorting
+}
+
+void pinjam_buku(){
+    // Membuat fungsi untuk meminjam buku
+}
+
+void admin(){
+    string password;
+    cout << "Masukkan password: ";
+    getline(cin, password);
+    getline(cin, password);
+
+    if(password == "12345"){
+        isTrue = true;
+    } else {
+        isTrue = false;
+    }
+}
+
+void edit_data(){
+    admin();
+    if(isTrue){
+        // Membuat edit data
+    }
+}
+
+void printArray(){
+    for (int i = 1; i <= MAX; i++){
+        // Buatlah fungsi untuk menampilkan array dengan output tabel
+    }
+}
+
+int main(){
+    int pilihan;
+    
+    cout << "======================================================" << endl;
+    cout << "===== PEMROGRAMAN LANJUT   DATABOOK PERPUSTAKAAN =====" << endl;
+    cout << "=========  PEMROGRAMAN LANJUT - KELOMPOK 2  ==========" << endl;
+    cout << "========= FARIS MUNIR MAHDI  |  21081010064 ==========" << endl;
+    cout << "======================================================" << endl;
+
+    data_buku();
+    data_tags();
+    
+    while(1){
+        flag = 0;
+        isTrue = false;
+        cout << "Databook Perpustakaan" << endl;
+        cout << "1. Data Seluruh Buku" << endl;
+        cout << "2. Cari Buku" << endl;
+        cout << "3. Pinjam Buku" << endl;
+        cout << "4. Edit Data Buku (Admin Only)" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Masukkan pilihanmu: ";
+        cin >> pilihan;
+
+        switch(pilihan){
+            case 1: cout << endl;
+                    cout << "1. Data berurut berdasarkan judul buku" << endl;
+                    cout << "2. Data berurut berdasarkan penerbit" << endl;
+                    cout << "3. Data berurut berdasarkan tahun terbit" << endl;
+                    cout << "4. Data berurut berdasarkan rating" << endl;
+                    cout << "Masukkan pilihanmu: ";
+                    cin >> pilihan;
+
+                    switch(pilihan){
+                        case 1: sorting();
+                                cout << endl;
+                                break;
+                        case 2: sorting();
+                                cout << endl;
+                                break;
+                        case 3: sorting();
+                                cout << endl;
+                                break;
+                        case 4: sorting();
+                                cout << endl;
+                                break;
+
+                        default: cout << "Pilihanmu Tidak Tersedia!" << endl << endl;
+                    }
+                    break;
+            case 2: cout << endl;
+                    cout << "1. Mencari buku berdasarkan judul buku" << endl;
+                    cout << "2. Mencari buku berdasarkan penerbit" << endl;
+                    cout << "3. Mencari buku berdasarkan tahun terbit" << endl;
+                    cout << "4. Mencari buku berdasarkan tags" << endl;
+                    cout << "Masukkan pilihanmu: ";
+                    cin >> pilihan;
+
+                    switch (pilihan){
+                        case 1: searching();
+                                cout << endl;
+                                break;
+                        case 2: searching();
+                                cout << endl;
+                                break;
+                        case 3: searching();
+                                cout << endl;
+                                break;
+                        case 4: isTrue = true;
+                                searching_tags();
+                                cout << endl;
+                                break;
+                    
+                        default: cout << "Pilihanmu Tidak Tersedia!" << endl << endl;
+                    }
+                    break;
+            case 3: cout << endl;
+                    pinjam_buku();
+                    break;
+            case 4: cout << endl;
+                    cout << "1. Menambahkan buku baru ke dalam databook" << endl;
+                    cout << "2. Menghapus buku dari databook" << endl;
+                    cout << "3. Merubah elemen buku dari databook" << endl;
+                    cout << "Masukkan pilihanmu: ";
+                    cin >> pilihan;
+
+                    switch (pilihan){
+                        case 1: edit_data();
+                                cout << endl;
+                                break;
+                        case 2: edit_data();
+                                cout << endl;
+                                break;
+                        case 3: edit_data();
+                                cout << endl;
+                                break;
+                        default: cout << "Pilihanmu Tidak Tersedia!" << endl << endl;
+                    }
+                    break;
+            case 5: exit (0);
+
+            default: cout << "Pilihanmu Tidak Tersedia!" << endl << endl;
+        }
+    }
+    return 0;
 }
