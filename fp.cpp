7e10e08ba7ea1c746de9406f23d0b5a843a761d1
shintyadhitawputri <120816216+shintyadhitawputri@users.noreply.github.com>
2022-12-24 17:42:10 +0700

    Update fp.cpp

--- fp.cpp
+++ fp.cpp
@@ -1,143 +1,19 @@
 void data_buku(){
-    perpus[1].judul_buku = "Beginning Ethical Hacking with Kali Linux";
-    perpus[1].penerbit = "Apress";
-    perpus[1].penulis = "Sanjib Sinha";
-    perpus[1].tahun_terbit = 2018;
-    perpus[1].rating = 9.2;
-    perpus[1].status = "Tersedia";
+    string list_judul_buku[20]= {"Beginning Ethical Hacking with Kali Linux", "Akuntansi Pengantar 1", "A-Z Psikologi : Berbagai kumpulan topik Psikologi", "Bangsa gagal ; Mencari identitas kebangsaan", "The Hobbit: An Unexpected Journey", "Koala Kumal", "Negeri 5 Menara", "Satwa Terancam Bahaya", "TUILET", "Dear Nathan", "Perahu Kertas", "5 cm", "Rindu", "Memahami Film", "Sebuah Seni untuk Bersikap Bodo Amat", "Rapijali", "Why? Camera – Kamera", "Wingit", "Belantik", "Runtuhnya Kerajaan Hindu Jawa dan Berdirinya Negara-Negara Islam di Nusantara"};
+    string list_penerbit[20]= {"Apress", "Gava Media", "Andi Offset", "LKiS", "HarperCollins Children’s Books", "Gagas Media", "PT. Gramedia Pustaka Utama", "Pakar Raya", "Gradien Mediatama", "Best Media", "Bentang Pustaka", "PT. Grasindo, Yogyakarta", "Republika", "Homerian Pustaka", "Gramedia Widiasarana Indonesia", "Bentang Pustaka", "Elex Media Komputindo", "Elex Media Komputindo", "Gramedia", "LKiS"};
+    string list_penulis[20]= {"Sanjib Sinha", "Supardi", "Zainul Anwar", "Nasruddin Anshoriy", "Paddy Kempshall", "Raditya Dika", "Ahmad Fuadi", "Jen Green", "Oben Cedric", "Erisca Febrianti", "Dewi Lestari (Dee)", "Dhonny Dhirgantoro", "Tere Liye", "Himawan Pratists", "Mark Manson", "Dee Lestari", "Yearimdang", "Sara Wijayanto", "Ahmad Tohari", "Slamet Muljana"};   
+    int list_tahun[20]= {2018, 2009, 2012, 2008, 2013, 2015, 2006, 2009, 2016, 2017, 2010, 2005, 2014, 2008, 2005, 2021, 2021, 2020, 2001, 2007};
+    float list_rating[20]= {9.2, 9.0, 8.8, 9.0, 2.6, 7.5, 7.8, 9.4, 9.0, 6.5, 9.7, 9.9, 8.2, 8.5, 8.9, 7.4, 8.1, 6.3, 8.1, 9.3};
+    string list_status[20]= {"Tersedia", "Tersedia", "Tidak Tersedia", "Tersedia", "Tidak Tersedia", "Tersedia", "Tersedia", "Tersedia", "Tersedia", "Tidak Tersedia", "Tersedia", "Tersedia", "Tersedia", "Tersedia", "Tidak Tersedia", "Tersedia", "Tersedia", "Tersedia", "Tersedia", "Tidak Tersedia"};
 
-    perpus[2].judul_buku = "Akuntansi Pengantar 1";
-    perpus[2].penerbit = "Gava Media";
-    perpus[2].penulis = "Supardi";
-    perpus[2].tahun_terbit = 2009;
-    perpus[2].rating = 9.0;
-    perpus[2].status = "Tersedia";
-
-    perpus[3].judul_buku = "A-Z Psikologi : Berbagai kumpulan topik Psikologi";
-    perpus[3].penerbit = "Andi Offset";
-    perpus[3].penulis = "Zainul Anwar";
-    perpus[3].tahun_terbit = 2012;
-    perpus[3].rating = 8.8;
-    perpus[3].status = "Tersedia";
-
-    perpus[4].judul_buku = "Bangsa gagal ; Mencari identitas kebangsaan";
-    perpus[4].penerbit = "LKiS";
-    perpus[4].penulis = "Nasruddin Anshoriy";
-    perpus[4].tahun_terbit = 2008;
-    perpus[4].rating = 9.0;
-    perpus[4].status = "Tersedia";
-
-    perpus[5].judul_buku = "The Hobbit: An Unexpected Journey";
-    perpus[5].penerbit = "HarperCollins Children’s Books";
-    perpus[5].penulis = "Paddy Kempshall";
-    perpus[5].tahun_terbit = 2013;
-    perpus[5].rating = 9.4;
-    perpus[5].status = "Tersedia";
-
-    perpus[6].judul_buku = "Koala Kumal";
-    perpus[6].penerbit = "Gagas Media";
-    perpus[6].penulis = "Raditya Dika";
-    perpus[6].tahun_terbit = 2015;
-    perpus[6].rating = 8.2;
-    perpus[6].status = "Tersedia";
-
-    perpus[7].judul_buku = "Negeri 5 Menara";
-    perpus[7].penerbit = "PT. Gramedia Pustaka Utama";
-    perpus[7].penulis = "Ahmad Fuadi";
-    perpus[7].tahun_terbit = 2012;
-    perpus[7].rating = 9.4;
-    perpus[7].status = "Tersedia";
-
-    perpus[8].judul_buku = "Satwa Terancam Bahaya";
-    perpus[8].penerbit = "Pakar Raya";
-    perpus[8].penulis = "Jen Green";
-    perpus[8].tahun_terbit = 2006;
-    perpus[8].rating = 9.0;
-    perpus[8].status = "Tersedia";
-
-    perpus[9].judul_buku = "TUILET";
-    perpus[9].penerbit = "Gradien Mediatama";
-    perpus[9].penulis = "Oben Cedric";
-    perpus[9].tahun_terbit = 2009;
-    perpus[9].rating = 8.4;
-    perpus[9].status = "Tersedia";
-
-    perpus[10].judul_buku = "Dear Nathan";
-    perpus[10].penerbit = "Best Media";
-    perpus[10].penulis = "Erisca Febrianti";
-    perpus[10].tahun_terbit = 2016;
-    perpus[10].rating = 9.7;
-    perpus[10].status = "Tersedia";
-
-    perpus[11].judul_buku = "Perahu Kertas";
-    perpus[11].penerbit = "Bentang Pustaka";
-    perpus[11].penulis = "Dewi Lestari (Dee)";
-    perpus[11].tahun_terbit = 2010;
-    perpus[11].rating = 9.5;
-    perpus[11].status = "Tersedia";
-
-    perpus[12].judul_buku = "5 cm";
-    perpus[12].penerbit = "PT. Grasindo, Yogyakarta";
-    perpus[12].penulis = "Dhonny Dhirgantoro";
-    perpus[12].tahun_terbit = 2005;
-    perpus[12].rating = 9.9;
-    perpus[12].status = "Tersedia";
-
-    perpus[13].judul_buku = "Rindu";
-    perpus[13].penerbit = "Republika";
-    perpus[13].penulis = "Tere Liye";
-    perpus[13].tahun_terbit = 2014;
-    perpus[13].rating = 8.2;
-    perpus[13].status = "Tersedia";
-
-    perpus[14].judul_buku = "Memahami Film";
-    perpus[14].penerbit = "Homerian Pustaka";
-    perpus[14].penulis = "Himawan Pratists";
-    perpus[14].tahun_terbit = 2008;
-    perpus[14].rating = 8.5;
-    perpus[14].status = "Tersedia";
-
-    perpus[15].judul_buku = "Sebuah Seni untuk Bersikap Bodo Amat";
-    perpus[15].penerbit = "Gramedia Widiasarana Indonesia";
-    perpus[15].penulis = "Mark Manson";
-    perpus[15].tahun_terbit = 2005;
-    perpus[15].rating = 8.9;
-    perpus[15].status = "Tersedia";
-
-    perpus[16].judul_buku = "Rapijali";
-    perpus[16].penerbit = "Bentang Pustaka";
-    perpus[16].penulis = "Dee Lestari";
-    perpus[16].tahun_terbit = 2021;
-    perpus[16].rating = 8.7;
-    perpus[16].status = "Tersedia";
-
-    perpus[17].judul_buku = "Why? Camera – Kamera";
-    perpus[17].penerbit = "Elex Media Komputindo";
-    perpus[17].penulis = "Yearimdang";
-    perpus[17].tahun_terbit = 2021;
-    perpus[17].rating = 8.7;
-    perpus[17].status = "Tersedia";
-
-    perpus[18].judul_buku = "Wingit";
-    perpus[18].penerbit = "Elex Media Komputindo";
-    perpus[18].penulis = "Sara Wijayanto";
-    perpus[18].tahun_terbit = 2020;
-    perpus[18].rating = 8.3;
-    perpus[18].status = "Tersedia";
-
-    perpus[19].judul_buku = "Belantik";
-    perpus[19].penerbit = "Gramedia";
-    perpus[19].penulis = "Ahmad Tohari";
-    perpus[19].tahun_terbit = 2001;
-    perpus[19].rating = 8.0;
-    perpus[19].status = "Tersedia";
-
-    perpus[20].judul_buku = "Runtuhnya Kerajaan Hindu Jawa dan Berdirinya Negara-Negara Islam di Nusantara";
-    perpus[20].penerbit = "LKiS";
-    perpus[20].penulis = "Slamet Muljana";
-    perpus[20].tahun_terbit = 2007;
-    perpus[20].rating = 8.2;
-    perpus[20].status = "Tersedia";
+  for (int i=0; i<MAX; i++){
+        perpus[i].judul_buku = list_judul_buku[i];
+        perpus[i].penerbit = list_penerbit[i];
+        perpus[i].penulis = list_penulis[i];
+        perpus[i].tahun_terbit = list_tahun[i];
+        perpus[i].rating = list_rating[i];
+        perpus[i].status = list_status[i];
+    }
 }
 
 void data_tags(){
