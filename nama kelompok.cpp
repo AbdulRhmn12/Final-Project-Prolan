3031673b6c5516319a8db4c1e407c0dfb4ce9a06
nadhiarama <121210688+nadhiarama@users.noreply.github.com>
2022-12-26 18:08:11 +0700

    Add files via upload

---
+++ nama kelompok.cpp
@@ -0,0 +1,12 @@
+int main(){
+    int pilihan;
+    int mandek=1;
+    cout << "==========================================================================" << endl;
+    cout << "=============== PEMROGRAMAN LANJUT   DATABOOK PERPUSTAKAAN ===============" << endl;
+    cout << "===============  PEMROGRAMAN LANJUT - KELOMPOK 2  ========================" << endl;
+    cout << "=========  ABDUL RAHMAN NURUDDIN                 |  21081010058 ==========" << endl;
+    cout << "=========  NADHIA RIZQY AMALIA                   |  21081010063 ==========" << endl;
+    cout << "=========  FARIS MUNIR MAHDI                     |  21081010064 ==========" << endl;
+    cout << "=========  SHINTYADHITA WIRAWAN PUTRI            |  21081010074 ==========" << endl;
+    cout << "=========  DIMAS ARYO HERLAMBANG PUTRO DANINDRO  |  21081010078 ==========" << endl;
+    cout << "==========================================================================" << endl;
