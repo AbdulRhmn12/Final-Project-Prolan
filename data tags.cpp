49534675796cb25401dec335dc2990483e3b57a8
nadhiarama <121210688+nadhiarama@users.noreply.github.com>
2022-12-26 18:06:23 +0700

    Add files via upload

---
+++ data tags.cpp
@@ -0,0 +1,28 @@
+void data_tags(){
+    SEO.push_back({"Beginning Ethical Hacking with Kali Linux", "informatika, hacking, cyber security, belajar linux, linux", 9.2});
+    SEO.push_back({"Beginning Ethical Hacking with Kali Linux", "informatika, hacking, cyber security, belajar linux, linux", 9.2});
+    SEO.push_back({"Akuntansi Pengantar 1", "akuntansi, pengantar akuntansi", 9.0});
+    SEO.push_back({"A-Z Psikologi : Berbagai kumpulan topik Psikologi", "psikologi, topik psikologi, kesehatan mental", 8.8});
+    SEO.push_back({"Bangsa gagal ; Mencari identitas kebangsaan", "karakteristik bangsa, nasionalisme, politik", 9.0});
+    SEO.push_back({"The Hobbit: An Unexpected Journey", "gandalf, the hobbit, fiksi, petualangan, dongeng, fairytale", 6.6});
+
+    SEO.push_back({"Koala Kumal", "fiksi, humor, cerita lucu, raditya dika", 7.5});
+    SEO.push_back({"Negeri 5 Menara", "fiksi, persahabatan, 5 menara", 7.8});
+    SEO.push_back({"Satwa Terancam Bahaya", "hewan, satwa, animal welfare", 9.4});
+    SEO.push_back({"TUILET", "komedi, humor, fiksi, cerita lucu", 9.0});
+    SEO.push_back({"Dear Nathan", "romantis, fiksi, kisah remaja", 6.5});
+
+    SEO.push_back({"Perahu Kertas", "persahabatan, romantis, fiksi", 9.7});
+    SEO.push_back({"5 cm", "persahabatan, fiksi, petualangan, harapan", 9.9});
+    SEO.push_back({"Rindu", "fiksi, tere liye", 8.2});
+    SEO.push_back({"Memahami Film", "film, movie, tentang film, paham film", 8.5});
+    SEO.push_back({"Sebuah Seni untuk Bersikap Bodo Amat", "self improvement, motivasi, psikologi sosial", 8.9});
+
+    SEO.push_back({"Rapijali", "perjuangan remaja, cerita musik", 7.4});
+    SEO.push_back({"Why? Camera – Kamera", "kamera, belajar kamera, fotografi, definisi kamera", 8.1});
+    SEO.push_back({"Wingit", "horor, cerita horor, hantu, kisah horor", 6.3});
+    SEO.push_back({"Belantik", "cerita perjalanan hidup, fiksi", 8.1});
+    SEO.push_back({"Runtuhnya Kerajaan Hindu Jawa dan Berdirinya Negara-Negara Islam di Nusantara", "sejarah, kerajaan, kerajaan Indonesia", 9.3});
+
+  
+}
